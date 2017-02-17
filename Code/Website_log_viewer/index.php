0a5957d54817658843cc18c62ad743d605524dab
HishamR <ramishhisham@gmail.com>
2017-02-17 14:41:31 +0100

    Updated website log/event viewer
    Various changes, including inline video/images

--- Code/Website_log_viewer/index.php
+++ Code/Website_log_viewer/index.php
@@ -1,4 +1,24 @@
 <?php
+
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+
+function display_debug($key, $value, $debug=false) {
+  if($debug){
+    echo ''.$key . " = ";
+    switch (gettype($value)) {
+      case 'string' :
+        echo $value;
+        break;
+      case 'array' :
+      case 'object' :
+      default :
+        echo '';
+        print_r($value);
+        echo ''; 
+        break;
+} } }
+
 $db_host = 'localhost'; // Server Name
 $db_user = 'root'; // Username
 $db_pass = '63AyvMSZec'; // Password
@@ -10,13 +30,56 @@ if (!$conn) {
 }
 
 $sql = 'SELECT * 
-		FROM logs';
+		FROM logs
+        ORDER BY log_id DESC';
 		
 $query = mysqli_query($conn, $sql);
 
 if (!$query) {
 	die ('SQL Error: ' . mysqli_error($conn));
 }
+
+$test = 0;
+$arrayVals = array();
+while ($row = mysqli_fetch_array($query)){
+    $arrayVals[] = $row;
+}
+
+if(isset($_GET['update']))
+{
+    $value = $_GET['update'];
+    $id = $_GET['info'];
+    
+   
+    display_debug('Value: ',$test, 1);
+    $test = $test +1;
+    //display_debug('<br> ID: ',$id, 1);
+    
+    $a= "UPDATE logs SET verified_as_crash='$value' WHERE log_id=$id";
+    $b = mysqli_query($conn, $a);
+    
+    if (!$b) {
+	   die ('SQL Error: ' . mysqli_error($conn));
+    }
+    
+   
+    mysqli_close($conn);
+    //echo "<meta http-equiv='refresh' content='0'>";
+    header("Location: http://188.166.72.14/index.php?caller=somevalue");
+    //header('Location: '.$_SERVER['REQUEST_URI']);
+    //$value = $_POST['name'];
+    //$id = $_POST['id'];
+    
+    
+}    
+
+if( isset( $_GET["caller"] ) && $_GET["caller"] == "somevalue" ) {
+    // I'm using Location because this will remove the get value
+    header( "Location: index.php" );
+    exit;
+}
+
+
 ?>
 <html>
 <head>
@@ -103,37 +166,113 @@ if (!$query) {
 	</style>
 </head>
 <body>
+    
 	<h1>Q6115-E DATABASE FOR REGISTERED CRASH EVENTS</h1>
 	<table class="data-table">
-		<caption class="title">Crash Data</caption>
+		<caption class="title">UNVERIFIED CRASH DATA</caption>
 		<thead>
 			<tr>
 				<th>NO</th>
 				<th>VIDEO LINK</th>
+                <th>IMAGE LINK</th>
 				<th>LOCATION</th>
 				<th>DATE</th>
 				<th>STATUS</th>
                 <th>CAMERA ID</th>
+                <th>REPORT INCIDENT</th>
 			</tr>
 		</thead>
 		<tbody>
 		<?php
-		$no 	= 1;
-		$total 	= 0;
-		while ($row = mysqli_fetch_array($query))
-		{
-			$amount  = $row['amount'] == 0 ? '' : number_format($row['amount']);
-			echo '<tr>
-					<td>'.$no.'</td>
-					<td>'.$row['video_link'].'</td>
-					<td>'.$row['location'].'</td>
-					<td>'.$row['time_date'] . '</td>
-					<td>'.$row['verified_as_crash'].'</td>
-                    <td>'.$row['cam_id'].'</td>
-				</tr>';
-			$no++;
-		}?>
+
+            $len = count($arrayVals);
+            for($x = 0; $x < $len; $x++){
+                $row = $arrayVals[$x];
+                
+                if($row['verified_as_crash']=='UNVERIFIED'){
+                    
+                    echo 
+                        '<tr>
+                            <td>'.$row['log_id'].'</td>
+				            <td><video width="320" height="180" controls src="'.$row['video_link'].'"></video></td>
+                            <td><img height="180" width="320" src="'.$row['img_link'].'"></td>
+				            <td>'.$row['location'].'</td>
+				            <td>'.$row['time_date'] . '</td>
+                            <td>'.$row['verified_as_crash'].'</td>
+                            <td>'.$row['cam_id'].'</td>
+                            <td> ';?>     
+                        
+                            <form method="get" action="<?php $_PHP_SELF ?> ">
+                                
+                                <input  name  = "info" 
+                                        type  = "hidden" 
+                                        id    = "1" 
+                                        value = "<?php echo $row['log_id']; ?>"/>
+                                
+                                <input  name  = "update" 
+                                        type  = "submit" 
+                                        id    = "15" 
+                                        value = "POSITIVE"/>
+                                
+                                <input  name  = "update" 
+                                        type  = "submit" 
+                                        id    = "15" 
+                                        value = "NEGATIVE"/>
+                            </form>
+                            
+                            <?php
+                            
+                            '</td>
+                        </tr>';
+
+                }
+
+            }
+        ?>
+            
 		</tbody>
 	</table>
+    
+	<table class="data-table">
+		<caption class="title">CRASH DATA LOGS</caption>
+		<thead>
+			<tr>
+				<th>NO</th>
+				<th>VIDEO LINK</th>
+                <th>IMAGE LINK</th>
+				<th>LOCATION</th>
+				<th>DATE</th>
+				<th>STATUS</th>
+                <th>CAMERA ID</th>
+                
+			</tr>
+		</thead>
+		<tbody>
+		<?php
+
+            $len = count($arrayVals);
+            for($x = 0; $x < $len; $x++){
+                $row = $arrayVals[$x];
+                
+                if($row['verified_as_crash']!='UNVERIFIED'){
+                    echo 
+                    
+                    '<tr>
+                        <td>'.$row['log_id'].'</td>
+				        <td><video width="320" height="180" controls src="'.$row['video_link'].'"></video></td>
+                        <td><img height="180" width="320" src="'.$row['img_link'].'"></td>
+				        <td>'.$row['location'].'</td>
+				        <td>'.$row['time_date'] . '</td>
+				        <td>'.$row['verified_as_crash'].'</td>
+                        <td>'.$row['cam_id'].'</td>
+				    </tr>';
+                }
+
+            }
+        ?>
+            
+		</tbody>
+	</table>
+
 </body>
 </html>
